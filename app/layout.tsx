8ed7eba1e9e783425bf0ebd32e6e94f152928ed9
Niels Grootscholten <niels.grootscholten@gmail.com>
2025-03-15 11:05:09 +1300

    Update app title

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
+  title: 'Timezone converter',
+  description: 'Timezone converter to help plan meetings.',
   generator: 'v0.dev',
 }
 
